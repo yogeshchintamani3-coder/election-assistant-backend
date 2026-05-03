babb5db65c8d57d2cc25b53cc60eb22b79df40b3
yogeshchintamani3-coder <yogesh.chintamani@kitaboo.com>
2026-05-03 16:05:54 +0530

    env update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  
   production: true,
   apiBaseUrl: 'https://election-assistant-backend-0hkv.onrender.com'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  
   production: false,
   apiBaseUrl: 'https://election-assistant-backend-0hkv.onrender.com'
 };
